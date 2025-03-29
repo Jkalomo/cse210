8d00e3cb3c3c88b9ace868d29480bb710643eda5
jkalomo <jkalomo@byupathway.edu>
2025-03-29 20:09:21 +0100

    Commit

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -18,7 +18,7 @@ class Address
 
     public bool IsInZimbabwe()
     {
-        return _country.ToLower() == "zimbabwe";
+        return _country.Equals("Zimbabwe", StringComparison.OrdinalIgnoreCase);
     }
 
     public string GetFullAddress()
@@ -43,15 +43,8 @@ class Customer
         return _address.IsInZimbabwe();
     }
 
-    public string GetName()
-    {
-        return _name;
-    }
-
-    public string GetShippingAddress()
-    {
-        return _address.GetFullAddress();
-    }
+    public string GetName() => _name;
+    public string GetShippingAddress() => _address.GetFullAddress();
 }
 
 class Product
@@ -69,15 +62,8 @@ class Product
         _quantity = quantity;
     }
 
-    public double GetTotalCost()
-    {
-        return _price * _quantity;
-    }
-
-    public string GetProductInfo()
-    {
-        return $"{_name} (ID: {_productId})";
-    }
+    public double GetTotalCost() => _price * _quantity;
+    public string GetProductInfo() => $"{_name} (ID: {_productId})";
 }
 
 class Order
@@ -91,10 +77,7 @@ class Order
         _products = new List<Product>();
     }
 
-    public void AddProduct(Product product)
-    {
-        _products.Add(product);
-    }
+    public void AddProduct(Product product) => _products.Add(product);
 
     public double GetTotalCost()
     {
@@ -109,12 +92,12 @@ class Order
 
     public string GetPackingLabel()
     {
-        string label = "Packing List:\n";
+        var label = "Packing List:\n";
         foreach (var product in _products)
         {
-            label += "- " + product.GetProductInfo() + "\n";
+            label += $"- {product.GetProductInfo()}\n";
         }
-        return label;
+        return label.TrimEnd();
     }
 
     public string GetShippingLabel()
@@ -127,36 +110,36 @@ class Program
 {
     static void Main()
     {
-        // Create Addresses in Zimbabwe
-        Address addr1 = new Address("123 Samora Machel Ave", "Harare", "Harare Province", "Zimbabwe");
-        Address addr2 = new Address("45 Joshua Nkomo St", "Bulawayo", "Bulawayo Province", "Zimbabwe");
+        // Create Addresses
+        var addr1 = new Address("123 Samora Machel Ave", "Harare", "Harare Province", "Zimbabwe");
+        var addr2 = new Address("45 Joshua Nkomo St", "Bulawayo", "Bulawayo Province", "Zimbabwe");
 
         // Create Customers
-        Customer cust1 = new Customer("Tafadzwa Moyo", addr1);
-        Customer cust2 = new Customer("Chipo Ndlovu", addr2);
+        var cust1 = new Customer("Tafadzwa Moyo", addr1);
+        var cust2 = new Customer("Chipo Ndlovu", addr2);
 
         // Create Products
-        Product prod1 = new Product("Laptop", "L123", 1200.00, 1);
-        Product prod2 = new Product("Mouse", "M456", 25.00, 2);
-        Product prod3 = new Product("Keyboard", "K789", 75.00, 1);
-        Product prod4 = new Product("Monitor", "D222", 300.00, 1);
+        var prod1 = new Product("Laptop", "L123", 1200.00, 1);
+        var prod2 = new Product("Mouse", "M456", 25.00, 2);
+        var prod3 = new Product("Keyboard", "K789", 75.00, 1);
+        var prod4 = new Product("Monitor", "D222", 300.00, 1);
 
         // Create Orders
-        Order order1 = new Order(cust1);
+        var order1 = new Order(cust1);
         order1.AddProduct(prod1);
         order1.AddProduct(prod2);
 
-        Order order2 = new Order(cust2);
+        var order2 = new Order(cust2);
         order2.AddProduct(prod3);
         order2.AddProduct(prod4);
 
         // Display Order Details
         Console.WriteLine(order1.GetPackingLabel());
         Console.WriteLine(order1.GetShippingLabel());
-        Console.WriteLine("Total Cost: $" + order1.GetTotalCost());
-        Console.WriteLine("\n-------------------------\n");
+        Console.WriteLine($"Total Cost: ${order1.GetTotalCost():F2}\n");
+        Console.WriteLine("-------------------------\n");
         Console.WriteLine(order2.GetPackingLabel());
         Console.WriteLine(order2.GetShippingLabel());
-        Console.WriteLine("Total Cost: $" + order2.GetTotalCost());
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost():F2}");
     }
 }
